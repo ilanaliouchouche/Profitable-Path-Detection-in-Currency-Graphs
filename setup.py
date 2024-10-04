58f481ff647ee0a48a16e88dfc9f086bccd841d4
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2024-10-04 17:52:10 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages  # type: ignore
 
 setup(
-    name='FISC-AI',
+    name='Applied-Graph-Optimization-for-Forex-Market',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    author='Ilan Aliouchouche',
+    install_requires=[
+        'numpy==1.26.4',
+        'pandas==2.2.2',
+    ],
+    python_requires='>=3.10',
 )
