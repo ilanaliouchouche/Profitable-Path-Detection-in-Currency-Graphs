9d608a555308c3af3cf1c23c215f36589dcc753f
ilanaliouchouche <ilan.aliouchouche@universite-paris-saclay.fr>
2024-10-14 21:39:02 +0200

    Benchmark done. TODO: Benchmark Interface

--- src/algo_benchmark.py
+++ src/algo_benchmark.py
@@ -5,30 +5,43 @@ from src.currencygraph import CurrencyGraph, CurrencyNode, CurrencyEdge
 from typing import Sequence, Callable, Optional, Tuple, List
 from collections import defaultdict
 from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
 
 
 class AlgoBenchmark:
     """
-    The goal of this class is to compare the performance of the different
-    algorithms implemented in `src/optimal_paths.py`.
+    The goal of this class is to compare the performance (time and complexity)
+    of the different algorithms implemented in `src/optimal_paths.py`.
     """
 
-    def __init__(self) -> None:
-        self.studies = []
-
-    @staticmethod
-    def time_study(algorithms: Sequence[Callable[
-                       [CurrencyGraph, CurrencyNode,
-                        Optional[int]], Tuple[List[CurrencyNode], float]]],
-                   node_sizes: Sequence[int],
-                   num_trials: int = 10,
-                   scale: float = 0.4,
-                   seed: int = 42,
-                   n_passages_fn: Optional[Callable[
-                       [CurrencyGraph], int]] = None) -> defaultdict:
+    def __init__(self,
+                 seed: int = 42) -> None:
         """
-        Generates random currency graphs for each size in `node_sizes`, runs
-        the provided algorithms, and measures the execution time.
+        Initializes the benchmarking class.
+
+        ## Parameters:
+            `seed`: Seed for the random number generator.
+        """
+
+        self.seed = seed
+        np.random.seed(seed)
+        random.seed(seed)
+
+    def run(self,
+            algorithms: Sequence[Callable[
+                [CurrencyGraph, CurrencyNode, Optional[int],
+                 Optional[Callable[[CurrencyNode], None]],
+                 Optional[Callable[[CurrencyEdge], None]]],
+                Tuple[List[CurrencyNode], float]]],
+            node_sizes: Sequence[int],
+            num_trials: int = 10,
+            scale: float = 0.4,
+            n_passages_fn: Optional[Callable[[CurrencyGraph], int]] = None,
+            verbose: bool = False
+            ) -> defaultdict:
+        """
+        Runs the provided algorithms and measures both execution time and
+        complexity (nodes visited and edges traversed).
 
         ## Parameters:
             `algorithms`: List of algorithms to compare. Each algorithm should
@@ -43,10 +56,30 @@ class AlgoBenchmark:
             `n_passages_fn`: Optional function that takes a graph and returns
                              an integer (n_passages). If None, n_passages will
                              be set to (number of nodes - 1).
+            `verbose`: If True, print the progress of the benchmark.
+
+        ## Returns:
+            A dictionary containing the execution time and complexity results
+            for each algorithm.
+
+        ## Example:
+        ```py
+        from src.optimal_paths import simplified_dijkstra, brute_force
+
+        benchmark = AlgoBenchmark(seed=42)
+        results, complexity_results = benchmark.run(
+            algorithms=[simplified_dijkstra, brute_force],
+            node_sizes=[5, 10, 15],
+            num_trials=10,
+            scale=0.4
+        )
+        ```
         """
 
-        random.seed(seed)
+        np.random.seed(self.seed)
+        random.seed(self.seed)
         results = defaultdict(lambda: defaultdict(list))
+        complexity_results = defaultdict(lambda: defaultdict(list))
 
         for num_nodes in tqdm(node_sizes, desc="Simulating some graphs"):
             for _ in range(num_trials):
@@ -68,26 +101,182 @@ class AlgoBenchmark:
                               else num_nodes - 1)
 
                 for algo in algorithms:
+                    visited_nodes = set()
+                    traversed_edges = 0
+
+                    def node_callback(node):
+                        visited_nodes.add(node)
+
+                    def edge_callback(edge):
+                        nonlocal traversed_edges
+                        traversed_edges += 1
+
                     start_time = perf_counter()
 
                     try:
-                        algo(G, nodes[0], n_passages)
+                        algo(G, nodes[0], n_passages,
+                             node_callback=node_callback,
+                             edge_callback=edge_callback)
                     except TypeError:
-                        algo(G, nodes[0])
+                        algo(G, nodes[0], node_callback=node_callback,
+                             edge_callback=edge_callback)
 
                     elapsed_time = perf_counter() - start_time
+
                     results[algo.__name__][num_nodes].append(elapsed_time)
 
-        for num_nodes, timings in results.items():
-            print(f"Graph with {num_nodes} nodes:")
-            for algo, times in timings.items():
+                    complexity_results[algo.__name__][num_nodes].append({
+                        'nodes': len(visited_nodes),
+                        'edges': traversed_edges
+                    })
+
+        for algo, timings in results.items():
+            if verbose:
+                print(f"\n{algo} Results:")
+            for num_nodes, times in timings.items():
                 avg_time = np.mean(times)
                 ci_lower = np.percentile(times, 2.5)
                 ci_upper = np.percentile(times, 97.5)
-                print(f"  {algo} Avg Time: {avg_time:.6f}s "
-                      f"(95% CI: [{ci_lower:.6f}, {ci_upper:.6f}])\n")
 
-        return results
+                avg_nodes = np.mean([comp['nodes'] for comp in
+                                     complexity_results[algo][num_nodes]])
+                ci_nodes_low = np.percentile([comp['nodes'] for comp in
+                                              complexity_results[algo]
+                                              [num_nodes]], 2.5)
+                ci_nodes_up = np.percentile([comp['nodes'] for comp in
+                                             complexity_results[algo]
+                                             [num_nodes]], 97.5)
 
-    def run(self):
-        pass
+                avg_edges = np.mean([comp['edges'] for comp in
+                                     complexity_results[algo][num_nodes]])
+                ci_edges_low = np.percentile([comp['edges'] for comp in
+                                              complexity_results[algo]
+                                              [num_nodes]], 2.5)
+                ci_edges_up = np.percentile([comp['edges'] for comp in
+                                             complexity_results[algo]
+                                             [num_nodes]], 97.5)
+                if verbose:
+                    print(f"  {algo} Avg Time: {avg_time:.6f}s (95% CI: "
+                          f"[{ci_lower:.6f}, {ci_upper:.6f}])")
+                    print(f"  {algo} Avg Nodes Visited: {avg_nodes:.2f} "
+                          f"(95% CI: [{ci_nodes_low:.2f}, {ci_nodes_up:.2f}])")
+                    print(f"  {algo} Avg Edges Traversed: {avg_edges:.2f} "
+                          f"(95% CI: [{ci_edges_low:.2f}, {ci_edges_up:.2f}])")
+
+        return results, complexity_results
+
+    @staticmethod
+    def plot_time_with_ci(results: defaultdict) -> None:
+        """
+        Static method to plot execution time vs number of samples,
+        with confidence intervals.
+
+        ## Parameters:
+            `results`: Dictionary containing the execution times and
+                       confidence intervals.
+
+        ## Example:
+        ```py
+        from src.optimal_paths import simplified_dijkstra, brute_force
+
+        benchmark = AlgoBenchmark(seed=42)
+        results, _ = benchmark.run(
+            algorithms=[simplified_dijkstra, brute_force],
+            node_sizes=[5, 10, 15],
+            num_trials=10,
+            scale=0.4
+        )
+        AlgoBenchmark.plot_time_with_ci(results)
+        ```
+        """
+
+        plt.style.use('ggplot')
+
+        plt.figure(figsize=(10, 6))
+
+        for algo, timings in results.items():
+            node_sizes = sorted(timings.keys())
+            avg_times = [np.mean(timings[n]) for n in node_sizes]
+            ci_lowers = [np.percentile(timings[n], 2.5) for n in node_sizes]
+            ci_uppers = [np.percentile(timings[n], 97.5) for n in node_sizes]
+
+            plt.plot(node_sizes, avg_times, label=algo, marker='o')
+
+            plt.fill_between(node_sizes, ci_lowers, ci_uppers, alpha=0.2)
+
+        plt.xlabel("Number of Nodes (N)")
+        plt.ylabel("Execution Time (seconds)")
+        plt.title("Algorithm Performance: Execution Time vs Number of Nodes")
+        plt.legend()
+
+        plt.show()
+
+    @staticmethod
+    def plot_complexity_with_ci(complexity_results: dict) -> None:
+        """
+        Static method to plot complexity vs number of samples, with confidence
+        intervals. Plots two graphs: one for nodes visited and one for edges.
+
+        ## Parameters:
+            `complexity_results`: Dictionary containing the complexity results.
+
+        ## Example:
+        ```py
+        from src.optimal_paths import simplified_dijkstra, brute_force
+
+        benchmark = AlgoBenchmark(seed=42)
+        _, complexity_results = benchmark.run(
+            algorithms=[simplified_dijkstra, brute_force],
+            node_sizes=[5, 10, 15],
+            num_trials=10,
+            scale=0.4
+        )
+        AlgoBenchmark.plot_complexity_with_ci(complexity_results)
+        ```
+        """
+
+        plt.style.use('ggplot')
+
+        _, (node_ax, edge_ax) = plt.subplots(1, 2, figsize=(14, 6))
+
+        for algo, data in complexity_results.items():
+            node_sizes = sorted(data.keys())
+            avg_nodes = [np.mean(
+                [comp['nodes'] for comp in data[n]]) for n in node_sizes]
+            ci_nodes_lowers = [np.percentile(
+                [comp['nodes'] for comp in data[n]], 2.5) for n in node_sizes]
+            ci_nodes_uppers = [np.percentile(
+                [comp['nodes'] for comp in data[n]], 97.5) for n in node_sizes]
+
+            node_ax.plot(node_sizes, avg_nodes, label=algo, marker='o')
+            node_ax.fill_between(node_sizes, ci_nodes_lowers, ci_nodes_uppers,
+                                 alpha=0.2)
+
+        node_ax.set_xlabel("Number of Nodes (N)")
+        node_ax.set_ylabel("Nodes Visited")
+        node_ax.set_title("Algorithm Complexity: Nodes Visited vs Number of "
+                          "Nodes")
+        node_ax.legend()
+
+        for algo, data in complexity_results.items():
+            node_sizes = sorted(data.keys())
+            avg_edges = [np.mean(
+                [comp['edges'] for comp in data[n]]) for n in node_sizes]
+            ci_edges_lowers = [np.percentile(
+                [comp['edges'] for comp in data[n]], 2.5) for n in node_sizes]
+            ci_edges_uppers = [np.percentile(
+                [comp['edges'] for comp in data[n]], 97.5) for n in node_sizes]
+
+            edge_ax.plot(node_sizes, avg_edges, label=algo, marker='o')
+            edge_ax.fill_between(node_sizes, ci_edges_lowers, ci_edges_uppers,
+                                 alpha=0.2)
+
+        edge_ax.set_xlabel("Number of Nodes (N)")
+        edge_ax.set_ylabel("Edges Traversed")
+        edge_ax.set_title("Algorithm Complexity: Edges Traversed vs Number of "
+                          "Nodes")
+        edge_ax.legend()
+
+        plt.tight_layout()
+
+        plt.show()
--- src/optimal_paths.py
+++ src/optimal_paths.py
@@ -1,9 +1,12 @@
-from src.currencygraph import CurrencyGraph, CurrencyNode
-from typing import List, Tuple
+from src.currencygraph import CurrencyGraph, CurrencyNode, CurrencyEdge
+from typing import List, Tuple, Callable
+import numpy as np
 
 
 def brute_force(G: CurrencyGraph,
-                start_currency: CurrencyNode
+                start_currency: CurrencyNode,
+                node_callback: Callable[[CurrencyNode], None] = None,
+                edge_callback: Callable[[CurrencyEdge], None] = None
                 ) -> Tuple[List[CurrencyNode], float]:
     """
     Brute force algorithm to find all possible cycles that start and end at
@@ -20,6 +23,7 @@ def brute_force(G: CurrencyGraph,
 
     ## Example
     ```py
+    from src.currencygraph import CurrencyGraph, CurrencyNode, CurrencyEdge
     nodes = [CurrencyNode('E'), CurrencyNode('D'),
              CurrencyNode('L'), CurrencyNode('F')]
     edges = [CurrencyEdge(self.nodes[0], self.nodes[0], 1.0),
@@ -49,9 +53,15 @@ def brute_force(G: CurrencyGraph,
     while stack:
         current_node, path, current_profit = stack.pop()
 
+        if node_callback:
+            node_callback(current_node)
+
         for edge in G.get_edges_from_source(current_node):
             next_node = edge.target
             new_profit = current_profit * edge.weight
+
+            if edge_callback:
+                edge_callback(edge)
 
             if next_node == start_currency and len(path) > 1:
                 all_cycles_with_profits.append((path + [start_currency],
@@ -62,9 +72,73 @@ def brute_force(G: CurrencyGraph,
     return max(all_cycles_with_profits, key=lambda x: x[1])
 
 
+def log_brute_force(G: CurrencyGraph,
+                    start_currency: CurrencyNode
+                    ) -> Tuple[List[CurrencyNode], float]:
+    """
+    Brute force algorithm to find all possible cycles that start and end at
+    the given start_currency in a currency graph while calculating their
+    profits using logarithmic transformation of edge weights to turn
+    multiplications into additions.
+
+    ## Parameters:
+        `G`: The currency graph to be analyzed.
+        `start_currency`: The currency node to start the cycle from.
+
+    ## Returns:
+        A list of currency nodes representing the most profitable cycle,
+        and the associated profit (in its original form, not log-transformed).
+
+    ## Example:
+    ```py
+    nodes = [CurrencyNode('E'), CurrencyNode('D'),
+             CurrencyNode('L'), CurrencyNode('F')]
+    edges = [CurrencyEdge(nodes[0], nodes[0], 1.0),
+             CurrencyEdge(nodes[1], nodes[1], 1.0),
+             CurrencyEdge(nodes[2], nodes[2], 1.0),
+             CurrencyEdge(nodes[3], nodes[3], 1.0),
+             CurrencyEdge(nodes[0], nodes[1], 1.19),
+             CurrencyEdge(nodes[1], nodes[0], 0.84),
+             CurrencyEdge(nodes[0], nodes[2], 1.33),
+             CurrencyEdge(nodes[2], nodes[0], 0.75),
+             CurrencyEdge(nodes[0], nodes[3], 1.62),
+             CurrencyEdge(nodes[3], nodes[0], 0.62),
+             CurrencyEdge(nodes[1], nodes[2], 1.12),
+             CurrencyEdge(nodes[2], nodes[1], 0.89),
+             CurrencyEdge(nodes[1], nodes[3], 1.37),
+             CurrencyEdge(nodes[3], nodes[1], 0.73),
+             CurrencyEdge(nodes[2], nodes[3], 1.22),
+             CurrencyEdge(nodes[3], nodes[2], 0.82)]
+    G = CurrencyGraph(nodes, edges)
+    output = log_brute_force(G, nodes[0])
+    ```
+    """
+
+    stack = [(start_currency, [start_currency], 0.0)]
+    all_cycles_with_profits = []
+
+    while stack:
+        current_node, path, current_log_profit = stack.pop()
+
+        for edge in G.get_edges_from_source(current_node):
+            next_node = edge.target
+            new_log_profit = current_log_profit + np.log(edge.weight)
+
+            if next_node == start_currency and len(path) > 1:
+                all_cycles_with_profits.append((path + [start_currency],
+                                                np.exp(new_log_profit)))
+            elif next_node not in path:
+                stack.append((next_node, path + [next_node], new_log_profit))
+
+    return max(all_cycles_with_profits, key=lambda x: x[1])
+
+
 def simplified_dijkstra(G: CurrencyGraph,
                         start_currency: CurrencyNode,
-                        n_passages: int
+                        n_passages: int,
+                        node_callback: Callable[[CurrencyNode], None] = None,
+                        edge_callback: Callable[[CurrencyEdge], None] = None,
+                        verbose: bool = False
                         ) -> Tuple[List[CurrencyNode], float]:
     """
     A crafted dijkstra algorithm to find the most profitable cycle that
@@ -75,6 +149,11 @@ def simplified_dijkstra(G: CurrencyGraph,
         `G`: The currency graph to analyze.
         `start_currency`: The currency node to start and end the cycle from.
         `n_passages`: The maximum number of exchanges (passages) allowed.
+        `node_callback`: A callback function to be called for each node
+                         visited.
+        `edge_callback`: A callback function to be called for each edge
+                         visited.l
+        `verbose`: If True, print the progress of the algorithm.
 
     ## Returns
         The most profitable cycle (list of CurrencyNodes)
@@ -82,6 +161,7 @@ def simplified_dijkstra(G: CurrencyGraph,
 
     ## Example
     ```py
+    from src.currencygraph import CurrencyGraph, CurrencyNode, CurrencyEdge
     nodes = [CurrencyNode('E'), CurrencyNode('D'),
              CurrencyNode('L'), CurrencyNode('F')]
     edges = [CurrencyEdge(self.nodes[0], self.nodes[0], 1.0),
@@ -110,18 +190,26 @@ def simplified_dijkstra(G: CurrencyGraph,
 
     best_paths = {node: [start_currency] for node in G.nodes}
 
+    node_callback(start_currency)
+
     for edge in G.get_edges_from_source(start_currency):
         lambda_values[edge.target] = edge.weight
         best_paths[edge.target] = [start_currency, edge.target]
 
+        edge_callback(edge)
+
     for k in range(1, n_passages):
-        print(f"Passage {k}")
+        if verbose:
+            print(f"Passage {k}")
 
         temp_lambda_values = lambda_values.copy()
         temp_best_paths = best_paths.copy()
 
         for node in G.nodes:
-            print(f"\tNode {node}")
+            node_callback(node)
+
+            if verbose:
+                print(f"\tNode {node}")
             max_value = lambda_values[node]
             best_path = best_paths[node]
 
@@ -129,12 +217,15 @@ def simplified_dijkstra(G: CurrencyGraph,
                 edge_weight = G.get_edge_weight(target_node, node)
                 if edge_weight > 0:
                     new_value = lambda_values[target_node] * edge_weight
-                    print(f"\t\t{target_node} -> {node}: "
-                          f"{lambda_values[target_node]}*"
-                          f"{edge_weight}={new_value}")
+                    if verbose:
+                        print(f"\t\t{target_node} -> {node}: "
+                              f"{lambda_values[target_node]}*"
+                              f"{edge_weight}={new_value}")
                     if new_value > max_value:
                         max_value = new_value
                         best_path = best_paths[target_node] + [node]
+
+                    edge_callback(CurrencyEdge(target_node, node, edge_weight))
 
             temp_lambda_values[node] = max_value
             temp_best_paths[node] = best_path
