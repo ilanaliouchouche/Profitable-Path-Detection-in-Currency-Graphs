45e69a8cc626cc9317e3a81543c53410795a04d2
Ilan <ilan.aliouchouche@universite-paris-saclay.fr>
2024-10-14 12:25:25 +0200

    log_brute_force done and works well

--- src/optimal_paths.py
+++ src/optimal_paths.py
@@ -1,5 +1,6 @@
 from src.currencygraph import CurrencyGraph, CurrencyNode
 from typing import List, Tuple
+import numpy as np
 
 
 def brute_force(G: CurrencyGraph,
@@ -60,6 +61,64 @@ def brute_force(G: CurrencyGraph,
                 stack.append((next_node, path + [next_node], new_profit))
 
     return max(all_cycles_with_profits, key=lambda x: x[1])
+
+
+def log_brute_force(G: CurrencyGraph,
+                    start_currency: CurrencyNode
+                    ) -> Tuple[List[CurrencyNode], float]:
+    """
+    Brute force algorithm to find all possible cycles that start and end at
+    the given start_currency in a currency graph while calculating their
+    profits using logarithmic transformation of edge weights to turn
+    multiplications into additions.
+
+    ## Parameters:
+        `G`: The currency graph to be analyzed.
+        `start_currency`: The currency node to start the cycle from.
+
+    ## Returns:
+        A list of currency nodes representing the most profitable cycle,
+        and the associated profit (in its original form, not log-transformed).
+
+    ## Example:
+    ```py
+    nodes = [CurrencyNode('E'), CurrencyNode('D'),
+             CurrencyNode('L'), CurrencyNode('F')]
+    edges = [CurrencyEdge(self.nodes[0], self.nodes[1], 1.19),
+             CurrencyEdge(self.nodes[1], self.nodes[0], 0.84),
+             CurrencyEdge(self.nodes[0], self.nodes[2], 1.33),
+             CurrencyEdge(self.nodes[2], self.nodes[0], 0.75),
+             CurrencyEdge(self.nodes[0], self.nodes[3], 1.62),
+             CurrencyEdge(self.nodes[3], self.nodes[0], 0.62),
+             CurrencyEdge(self.nodes[1], self.nodes[2], 1.12),
+             CurrencyEdge(self.nodes[2], self.nodes[1], 0.89),
+             CurrencyEdge(self.nodes[1], self.nodes[3], 1.37),
+             CurrencyEdge(self.nodes[3], self.nodes[1], 0.73),
+             CurrencyEdge(self.nodes[2], self.nodes[3], 1.22),
+             CurrencyEdge(self.nodes[3], self.nodes[2], 0.82)]
+    G = CurrencyGraph(nodes, edges)
+    output = log_brute_force(G, nodes[0])
+    ```
+    """
+
+    stack = [(start_currency, [start_currency], 0.0)]
+    all_cycles_with_profits = []
+
+    while stack:
+        current_node, path, current_log_profit = stack.pop()
+
+        for edge in G.get_edges_from_source(current_node):
+            next_node = edge.target
+            new_log_profit = current_log_profit + np.log(edge.weight)
+
+            if next_node == start_currency and len(path) > 1:
+                all_cycles_with_profits.append((path + [start_currency],
+                                                np.exp(new_log_profit)))
+            elif next_node not in path:
+                stack.append((next_node, path + [next_node], new_log_profit))
+
+    best_pair = max(all_cycles_with_profits, key=lambda x: x[1])
+    return best_pair[0], best_pair[1]
 
 
 def simplified_dijkstra(G: CurrencyGraph,
